b2353bb4e1f6f265d0a25cb17c581c799e1d8a3e
steve-fischer-200 <sfischer@upenn.edu>
2024-01-26 13:26:36 -0500

    adjust view

---
+++ lib/sql/Oracle/createView.sql
@@ -0,0 +1,28 @@
+CREATE VIEW VDI_DATASETS_@SUFFIX@.AvailableUserDatasets AS
+SELECT
+    v.dataset_id as user_dataset_id,
+    v.user_id,
+    d.type_name as type,
+    m.name,
+    m.description,
+    p.project
+FROM
+    VDI_CONTROL_@SUFFIX@.dataset_visibility v,
+    VDI_CONTROL_@SUFFIX@.dataset d,
+    VDI_CONTROL_@SUFFIX@.dataset_meta m,
+    VDI_CONTROL_@SUFFIX@.dataset_project p,
+    (SELECT dataset_id
+     FROM VDI_CONTROL_@SUFFIX@.dataset_install_message
+     WHERE install_type = 'meta'
+     AND status = 'complete'
+     INTERSECT
+     SELECT dataset_id
+     FROM VDI_CONTROL_@SUFFIX@.dataset_install_message
+     WHERE install_type = 'data'
+     AND status = 'complete'
+    ) i
+    WHERE v.dataset_id = i.dataset_id
+    and v.dataset_id = d.dataset_id
+    and v.dataset_id = m.dataset_id
+    and v.dataset_id = p.dataset_id
+    and d.is_deleted = 0
