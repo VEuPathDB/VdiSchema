412337616e67f1f789d35787ac191526996b71ba
steve-fischer-200 <sfischer@upenn.edu>
2026-05-06 15:12:46 -0400

    add variable tables

--- Main/lib/sql/Postgres/createEntityGraphTables.sql
+++ Main/lib/sql/Postgres/createEntityGraphTables.sql
@@ -51,5 +51,64 @@ GRANT SELECT ON VDI_DATASETS_:VAR1.EntityTypeGraph_sq TO gus_r;
 CREATE INDEX entitytypegraph_ix_1 ON VDI_DATASETS_:VAR1.entitytypegraph(study_stable_id, stable_id, parent_stable_id);
 CREATE INDEX entitytypegraph_ix_2 ON VDI_DATASETS_:VAR1.entitytypegraph(parent_stable_id);
 
+---------------------------------------------------------------------
+CREATE TABLE VDI_DATASETS_:VAR1.AttributeGraph
+(
+    stable_id character varying(255) NOT NULL,
+    study_stable_id character varying(255) NOT NULL,
+    bin_width_computed character varying(4),
+    bin_width_override character varying(1),
+    data_shape character varying(11),
+    data_type character varying(7),
+    definition text,
+    display_name text,
+    display_order numeric(3),
+    display_range_max character varying(16),
+    display_range_min character varying(16),
+    display_type character varying(20),
+    distinct_values_count numeric(10),
+    has_study_dependent_vocabulary numeric(1),
+    has_values numeric(10),
+    hidden character varying(64),
+    impute_zero numeric(1),
+    is_featured numeric(1),
+    is_merge_key numeric(1),
+    is_multi_valued numeric,
+    is_repeated numeric(1),
+    is_temporal numeric(1),
+    lower_quartile character varying(10),
+    mean character varying(16),
+    median character varying(10),
+    parent_stable_id character varying(255),
+    "precision" numeric(10),
+    provider_label text,
+    range_max character varying(10),
+    range_min character varying(10),
+    scale character varying(1),
+    unit character varying(1),
+    upper_quartile character varying(10),
+    variable_spec_to_impute_zeroes_for character varying(200),
+    vocabulary character varying(61),
+    weighting_variable_spec character varying(200),
+  FOREIGN KEY (study_stable_id) REFERENCES VDI_DATASETS_:VAR1.study(stable_id),
+  PRIMARY KEY (study_stable_id, stable_id)
+);
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON VDI_DATASETS_:VAR1.AttributeGraph TO vdi_w;
+GRANT SELECT ON VDI_DATASETS_:VAR1.AttributeGraph TO gus_r;
+
+----------------------------------------------------------------------
+
+CREATE TABLE VDI_DATASETS_:VAR1.AttributeCategoricalValue
+(
+    stable_id character varying(255) NOT NULL,
+    study_stable_id character varying(255) NOT NULL,
+    value text NOT NULL,
+  FOREIGN KEY (study_stable_id) REFERENCES VDI_DATASETS_:VAR1.study(stable_id),
+  PRIMARY KEY (study_stable_id, stable_id)
+);
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON VDI_DATASETS_:VAR1.AttributeCategoricalValue TO vdi_w;
+GRANT SELECT ON VDI_DATASETS_:VAR1.AttributeCategoricalValue TO gus_r;
 
 
